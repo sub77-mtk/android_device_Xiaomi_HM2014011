bb2a991f7393ddd4a889973d6e0895f18bb387a6
ferhung <ferhung27@gmail.com>
2016-08-20 22:11:32 +0800

    Add AAPT for building.

--- device_HM2014011.mk
+++ device_HM2014011.mk
@@ -163,6 +163,10 @@ PRODUCT_DEVICE := HM2014011
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
 PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory
